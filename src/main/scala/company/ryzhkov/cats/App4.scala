052ec03ed263ba136d11f29b3a858e670e76e621
StanislavRyzhkov <30403280+StanislavRyzhkov@users.noreply.github.com>
2020-01-20 22:32:49 +0700

    Update App4.scala

--- src/main/scala/company/ryzhkov/cats/App4.scala
+++ src/main/scala/company/ryzhkov/cats/App4.scala
@@ -2,8 +2,22 @@ package company.ryzhkov.cats
 
 import cats.Monad
 import cats.implicits._
+import cats.data.Kleisli
+import cats.effect.IO
 
 object App4 extends App {
-    val x = Monad[Option].map(3.some)(_ + 1)
-    println(x)
+  val x = Monad[Option].map(3.some)(_ + 1)
+  println(x)
+
+  val z: Kleisli[IO, Seq[Int], Seq[String]] =
+    Kleisli(e => IO(e.map(f => (f + 1).toString())))
+
+  val g = IO(List(3, 4))
+
+  val h = g.flatMap(z.run)
+
+  h.unsafeRunAsync {
+    case Right(value) => println(value)
+    case Left(value)  => println(value.getMessage)
+  }
 }
